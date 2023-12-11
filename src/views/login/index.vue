3c356b6e92e9d3c7eff249debacb8486660ccd3b
XL_Mr <643158351@qq.com>
2023-12-11 20:49:20 +0800

    登录页面list数组更新

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,11 +5,6 @@
 </template>
 <script setup lang='ts' name="login">
 import { ref } from 'vue'
-const list: string[] = [
-    '1',
-    '2',
-    '3',
-    '4'
-]
+const list: string[] = ['1', '2', '3', '4']
 </script>
 <style></style>
