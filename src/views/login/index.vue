3733c4e0d9858c6ae81d30559c0857e7312e5588
XL_Mr <643158351@qq.com>
2023-12-11 21:30:22 +0800

    ignore

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,5 +6,7 @@
 <script setup lang='ts' name="login">
 import { ref } from 'vue'
 const list: string[] = ['1', '2', '3', '4']
+const list1: string[] = ['1', '2', '3', '4']
+
 </script>
 <style></style>
