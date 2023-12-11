3be45620badb5e5a4d329084224d44512213ef1a
XL_Mr <643158351@qq.com>
2023-12-11 20:15:19 +0800

    main.ts内部顺序

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
 import router from './router'
-const pinia = createPinia()
 const app = createApp(App)
+const pinia = createPinia()
 app.use(ElementPlus)
 app.use(router)
 app.use(pinia)
