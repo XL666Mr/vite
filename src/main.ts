0e32ba2b0ee10c9afdb5c512f2f861bdb6c325db
XL_Mr <643158351@qq.com>
2023-12-11 20:25:44 +0800

    main.ts测试

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,11 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
 import router from './router'
-const app = createApp(App)
 const pinia = createPinia()
+
+const app = createApp(App)
+
+
 app.use(ElementPlus)
 app.use(router)
 app.use(pinia)
